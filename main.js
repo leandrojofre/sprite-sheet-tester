dfe61991faafd4e26187d29af930ed93f7eb8c88
Leandro Jofré <leandrotomasjofre@gmail.com>
2024-03-09 04:13:55 -0300

    Feat: Save system
    Each loaded sprite will be stored in history, where it can be reloaded and continued using with the same values ​​in the inputs, deleted or rewritten.

--- main.js
+++ main.js
@@ -18,6 +18,19 @@ function renderSprite() {
 	sprite.draw();
 }
 
+async function setSprite(spriteObj) {
+	let imgLoaded = false;
+	sprite = new Sprite(spriteObj);
+	sprite.img.onload = () => imgLoaded = true;
+	sprite.animationEnded = true;
+	
+	await new Promise((resolve) => {
+		let interval = setInterval(() => {
+			if (imgLoaded) resolve(renderSprite(), clearInterval(interval));
+		}, 100);
+	});
+}
+
 function showImage(event) {
 	let file = event.target.files[0];
 	
@@ -25,18 +38,15 @@ function showImage(event) {
 
 	let reader = new FileReader();
 	reader.onload = function (event) {
-		sprite = new Sprite({
-			src: event.target.result,
-			width: 64,
-			height: 64
-		});
-		sprite.animationEnded = true;
-		sprite.img.onload = () => renderSprite();
+		setSprite({src: event.target.result, width: 64, height: 64});
 	}
+	
 	reader.readAsDataURL(file);
 }
 
 function initializeInput() {
+	setHistoryItems();
+
 	let inputFile = document.getElementById("input-sprite");
 	inputFile.addEventListener("change", showImage);
 }
@@ -60,6 +70,7 @@ async function receiveForm(event) {
 	sprite.sy = (Number(elements["sprite-frame-row"].value) - 1) * sprite.sHeight;
 	sprite.animate = elements["can-animate"].checked;
 	
+	addImgToHistory();
 	sprite.setFrame(sprite.frameStart);
 	
 	if (sprite.animate) sprite.setAnimation();
@@ -132,7 +143,7 @@ function setInputNumberValue(e) {
 }
 
 function setMaxNumberOfRows(inputValue) {
-	// Maximum canvas width/height resolution in chrome: 32,767px.
+	// Maximum canvas img resolution in chrome: 32,767px.
 	const MAX_CAVAS_RESOLUTION = 32767;
 	const MAX_NUM_OF_ROWS = Math.floor(MAX_CAVAS_RESOLUTION / inputValue);
 
@@ -189,3 +200,148 @@ function keyUp(e) {
 window.addEventListener("load", initializeInput);
 window.addEventListener("keydown", keyDown);
 window.addEventListener("keyup", keyUp);
+
+function showHistoryItems() {
+	const $historyItems = document.getElementById("history-items");
+	let spriteItems = localStorage.getItem("sprites");
+	
+	if (spriteItems === undefined || spriteItems === null) return $historyItems.style.display = "none";
+	else spriteItems = JSON.parse(spriteItems);
+
+	if (spriteItems.length === 0)  return $historyItems.style.display = "none";
+
+	return $historyItems.style.display = "flex";
+};
+
+function setHistoryItems() {
+	let spriteItems = localStorage.getItem("sprites");
+	
+	if (spriteItems === undefined || spriteItems === null) return;
+	else spriteItems = JSON.parse(spriteItems);
+
+	const $historyItemsContainer = document.getElementById("history-items");
+	const HISTORY_ITEM_WIDTH = 128;
+
+	const generateItemContainer = (index, spriteObj) => {
+		const $div = document.createElement("div");
+		$div.className = "history-item";
+		$div.style.width = 128;
+		$div.style.height = 128;
+		
+		const $input = document.createElement("input");
+		$input.type = "button";
+		$input.value = "X";
+		$input.style.cursor = "pointer";
+		$input.style.width = "min-content";
+		$input.style.height = "min-content";
+		$input.style.zIndex = 1;
+		$input.style.alignSelf = "end";
+		$input.style.backgroundColor = "#473524";
+		$input.style.webkitTextFillColor = "#eadbac";
+		$input.style.textAlign = "center";
+		$input.style.border = "0px";
+		$input.historyIndex = index;
+		$input.onclick = (e) => deleteImgFromHistory(e.target.historyIndex);
+		$div.appendChild($input);
+		
+		const $img = document.createElement("img");
+		$img.src = spriteObj.src;
+		$img.id = index;
+		$img.style.position = "absolute";
+		$img.style.zIndex = 0;
+
+		$img.onload = () => $img.style.width = `${$img.width * (HISTORY_ITEM_WIDTH / spriteObj.sWidth)}px`;
+		$img.onclick = (e) => loadImgFromHistory(e.target.id);
+		$div.appendChild($img);
+		
+		return $div;
+	}
+	
+	$historyItemsContainer.innerHTML = "";
+
+	for (let i = 0; i < spriteItems.length; i++) 
+		$historyItemsContainer.appendChild(generateItemContainer(i, spriteItems[i]));
+
+	showHistoryItems();
+}
+
+async function loadImgFromHistory(index) {
+	const sprites = JSON.parse(localStorage.getItem("sprites"));
+
+	setSprite(sprites[index]);
+	
+	document.getElementById("sprite-size").value = sprite.width;
+	document.getElementById("sprite-frame-size").value = sprite.sWidth;
+	document.getElementById("sprite-frame-rate").value = sprite.frameSpeed;
+	document.getElementById("sprite-frame-start").value = sprite.frameStart + 1;
+	document.getElementById("sprite-frame-end").value = sprite.frameEnd;
+	document.getElementById("sprite-frame-row").value = sprite.sy + 1;
+	document.getElementById("can-animate").checked = sprite.animate;
+
+	setMaxNumberOfRows(sprite.sWidth);
+	sprite.setFrame(sprite.frameStart);
+
+	if (sprite.animate) sprite.setAnimation();
+}
+
+function removeDuplicatesFromArray(arr, filter) {
+	const filteredArr = [];
+	
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i];
+
+		if (filter(element, filteredArr)) continue;
+		else filteredArr.push(element);
+	}
+	
+	arr.splice(0, arr.length);
+	arr.push(...filteredArr);
+
+	return arr;
+}
+
+function addImgToHistory() {
+	if (sprite?.img === undefined) return alert("No sprite selected");
+
+	let spriteItems = localStorage.getItem("sprites");
+
+	if (spriteItems === undefined || spriteItems === null) spriteItems = [];
+	else spriteItems = JSON.parse(spriteItems);
+
+	spriteItems.push(sprite);
+
+	removeDuplicatesFromArray(spriteItems, (element, filteredArray) => {
+		let isDuplicate = true;
+		let sameImgSrc;
+
+		if (filteredArray.length == 0) return false;
+
+		for (let i = 0; i < filteredArray.length; i++) {
+			const elementFiltered = filteredArray[i];
+
+			for (const key of Object.keys(element)) {
+				if (element.src === elementFiltered.src) sameImgSrc = i;
+				if (element[key] === elementFiltered[key]) continue;
+				else {
+					isDuplicate = false;
+				}
+			}
+		}
+
+		if (sameImgSrc !== undefined === !isDuplicate) filteredArray.splice(sameImgSrc, 1);
+		
+		return isDuplicate;
+	});
+
+	localStorage.setItem("sprites", JSON.stringify(spriteItems));
+	setHistoryItems();
+}
+
+function deleteImgFromHistory(index) {
+	const spriteItems = JSON.parse(localStorage.getItem("sprites"));
+
+	spriteItems.splice(index, 1);
+	
+	localStorage.setItem("sprites", JSON.stringify(spriteItems));
+	setHistoryItems();
+}
